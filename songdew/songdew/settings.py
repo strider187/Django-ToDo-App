7bf49caf73e379149f93199d22e386142b573659
tarabacha <56684364+tarabacha@users.noreply.github.com>
2020-10-02 23:51:56 +0530

    Update settings.py
    Removed the security key left exposed.

--- songdew/songdew/settings.py
+++ songdew/songdew/settings.py
@@ -20,7 +20,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tq(#+-8o#)mznmtg)tp)-nj5*99c_oec=1m$6s71deo^y7u!b4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
